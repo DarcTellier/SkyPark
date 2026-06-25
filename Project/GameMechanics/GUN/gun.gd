3b8ba2e9a5095a29746ba7d37c51fc0bea58d6c1
unknown <darc.tellier@gmail.com>
2026-06-25 17:55:38 -0400

    back from camp switching to pc

--- Project/GameMechanics/GUN/gun.gd
+++ Project/GameMechanics/GUN/gun.gd
@@ -1,32 +1,27 @@
 extends Node2D
 
+@export_flags_2d_physics var shoot_collision_mask := 1
+
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
-		print("Mouse button detected: ", event.button_index, " pressed: ", event.pressed)
-
 		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			print("LEFT CLICK - shooting")
 			shoot_at_mouse()
 
 
 func shoot_at_mouse() -> void:
 	var mouse_pos := get_global_mouse_position()
-	print("Mouse world position: ", mouse_pos)
 
 	var query := PhysicsPointQueryParameters2D.new()
 	query.position = mouse_pos
+	query.collision_mask = shoot_collision_mask
 	query.collide_with_bodies = true
-	query.collide_with_areas = true
+	query.collide_with_areas = false
 
 	var hits := get_world_2d().direct_space_state.intersect_point(query, 32)
 
-	print("Hits found: ", hits.size())
-
 	for hit in hits:
 		var collider = hit.collider
-		print("Hit collider: ", collider.name)
 
-		if collider.has_method("toggle_spin"):
-			print("Toggling platform")
-			collider.toggle_spin()
+		if collider.has_method("shot"):
+			collider.shot(mouse_pos)
 			return
---
+++ Project/GameMechanics/Platform/Shootables/shootable_block.gd
@@ -0,0 +1,5 @@
+extends StaticBody2D
+
+
+func shot(hit_position: Vector2) -> void:
+	queue_free()
